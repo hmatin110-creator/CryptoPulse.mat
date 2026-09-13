c231e7bada887d59fe6554bccbc7b461c75220be
hmatin110-creator <hmatin110@gmail.com>
2026-09-13 23:33:36 +0330

    Update NewsRepository.kt

--- app/src/main/java/com/cryptopulse/app/NewsRepository.kt
+++ app/src/main/java/com/cryptopulse/app/NewsRepository.kt
@@ -12,6 +12,7 @@ import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
 import java.time.temporal.ChronoUnit
 import java.util.Locale
+import kotlin.math.roundToInt
 
 /**
 
