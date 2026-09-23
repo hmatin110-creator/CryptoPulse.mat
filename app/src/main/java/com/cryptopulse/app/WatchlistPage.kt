05f1470cfa094817e5d532d71083f6c3464672ec
hmatin110-creator <hmatin110@gmail.com>
2026-09-23 16:28:39 +0330

    Update WatchlistPage.kt

--- app/src/main/java/com/cryptopulse/app/WatchlistPage.kt
+++ app/src/main/java/com/cryptopulse/app/WatchlistPage.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.item
@@ -29,6 +30,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Locale
 
 @androidx.compose.runtime.Composable
