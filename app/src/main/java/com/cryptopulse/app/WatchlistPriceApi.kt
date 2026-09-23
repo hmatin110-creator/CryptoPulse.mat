b3e65fd1ade3fbf3de3969af55c14b1afbd0b06d
hmatin110-creator <hmatin110@gmail.com>
2026-09-23 13:38:42 +0330

    Update WatchlistPriceApi.kt

--- app/src/main/java/com/cryptopulse/app/WatchlistPriceApi.kt
+++ app/src/main/java/com/cryptopulse/app/WatchlistPriceApi.kt
@@ -1,1 +1,18 @@
+package com.cryptopulse.app
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WatchlistPriceApi {
+
+@GET("api/v3/ticker/price")
+suspend fun tickerPrice(
+    @Query("symbol") symbol: String
+): WatchlistPriceDto
+
+}
+
+data class WatchlistPriceDto(
+val symbol: String,
+val price: String
+)
